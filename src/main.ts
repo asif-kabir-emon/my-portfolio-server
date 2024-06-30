37b485547f49ec36db7f4d56ed47b9c70c13ed8a
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-06-30 21:07:25 +0600

    Update

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
   app.useGlobalPipes(
