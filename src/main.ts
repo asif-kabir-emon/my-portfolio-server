72fe6fc80d315fd46261f9973462973cac9e988d
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-06-30 21:41:06 +0600

    Add new cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
