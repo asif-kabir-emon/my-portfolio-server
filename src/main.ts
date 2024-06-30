868a892c074d71b42fa2a729c95ce1d178c73cb4
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-06-30 20:42:25 +0600

    Update main

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
